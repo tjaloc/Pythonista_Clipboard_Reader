cc7abc586ca70b2b64cebc0f05d01bfc19f1acc1
tjaloc <k.locke@gmx.de>
2024-11-18 12:41:41 +0100

    clipboard_reader.py : add read-button and speed/ rate adjustement
    # read button
    
    Allows to re-read the clipboard's content or read newly copied text
    
    # speech rate
    
    Adjust speed rate. New rate will be applied to next text block (usually next sentence).
    
    # Add attribute self.busy (bool)
    
    Speech acts async. To synchronize text view and speech ClipboardReader is busy while either is_speaking or preprocessing the clipboard content i.e. getting dominant language and a suitable voice.

--- clipboard_reader.py
+++ clipboard_reader.py
@@ -1,32 +1,50 @@
-from objc_util import ObjCClass, ns
-from preferences import VOICE_PREFERENCES, RATE
+from objc_util import ObjCClass, ns, create_objc_class#, ObjCInstance
+from preferences import RATE, VOICE_PREFERENCES
 
 class ClipboardReader:
-
     def __init__(self):
         self.voices = None
         self.rate = RATE
         self.language = None
         self.name = None
         self.id = None
+        self.current_text = ''
         
-        self.NSLinguisticTagger = ObjCClass('NSLinguisticTagger')
-        self.NSString = ObjCClass('NSString')
-        self.AVSpeechSynthesisVoice = ObjCClass('AVSpeechSynthesisVoice')
-        self.AVSpeechUtterance = ObjCClass('AVSpeechUtterance')
-        self.AVSpeechSynthesizer = ObjCClass('AVSpeechSynthesizer')
-        
-        self.SYNTHESIZER = self.AVSpeechSynthesizer.alloc().init()
+        self.SYNTHESIZER = ObjCClass('AVSpeechSynthesizer').alloc().init()
         self.UTTERANCE = None
-
+        
+        self.busy = False
+        self.setup_delegate()
+        
+    def setup_delegate(self):
+        """Helper to use ObjC's speech synthesizer's didFinish-method. When reader is finished reading its status changes to 'not busy'.
+        
+        Why not simply use self.SYNTHESIZER.isSpeaking()?
+        
+        The busy state includes preprocessing i.e. getting the dominant language and a voice.
+        """
+        reader = self
+        def speechSynthesizer_didFinishSpeechUtterance_(_self, _cmd, synthesizer, utterance):
+            reader.busy = False
+        
+        DelegateClass = create_objc_class(
+            'SpeechSynthDelegate',
+            methods=[speechSynthesizer_didFinishSpeechUtterance_],
+            protocols=['AVSpeechSynthesizerDelegate']
+        )
+        self.delegate = DelegateClass.alloc().init()
+        self.SYNTHESIZER.setDelegate_(self.delegate)
+        
     def who_is_speaking(self):
+        """Return name and language of current voice.
+        """
         return f'{self.name} ({self.language})'
         
     def detect_language(self, text: str) -> str:
         """Detect dominant language of a given Text.
         """
-        string = self.NSString.stringWithString_(text)
-        tagger = self.NSLinguisticTagger.alloc().initWithTagSchemes_options_(['Language'], 0)
+        string = ObjCClass('NSString').stringWithString_(text)
+        tagger = ObjCClass('NSLinguisticTagger').alloc().initWithTagSchemes_options_(['Language'], 0)
         tagger.setString_(string)
         detected_language = tagger.dominantLanguage()
     
@@ -35,28 +53,33 @@ class ClipboardReader:
     def get_voices(self, lng: str) -> list:
         """Get available voices in a given language.
         """
-        voices = self.AVSpeechSynthesisVoice.speechVoices()
+        voices = ObjCClass('AVSpeechSynthesisVoice').speechVoices()
         return [voice for voice in voices if str(voice.language()).startswith(lng)]
         
-    
-    def speak_with_voice(self, text, language_code, voice_identifier):
+    def speak_with_voice(self, text):
         """Replacement for speech.say() that also allows to choose a specific voice.
         """
-        self.UTTERANCE = self.AVSpeechUtterance.alloc().initWithString_(ns(text))
-        voice = self.AVSpeechSynthesisVoice.voiceWithIdentifier_(
-            ns(voice_identifier))
+        # create new UTTERANCE
+        self.UTTERANCE = ObjCClass('AVSpeechUtterance').alloc().initWithString_(ns(text))
+        self.current_text = str(self.UTTERANCE.valueForKey_("speechString"))
+        
+        # set params
+        voice = ObjCClass('AVSpeechSynthesisVoice').voiceWithIdentifier_(
+            ns(self.id))
         self.UTTERANCE.setVoice_(voice)
-        self.UTTERANCE.setRate_(max(0, min(self.rate, 1))) # sets rate to 0 <= rate <= 1
+        self.update_speed_rate()
+        
+        # speak
         self.SYNTHESIZER.speakUtterance_(self.UTTERANCE)
     
     def read_loud(self, content: str) -> None:
-        """ Read given text. The öanguage will be determined an a suitable voice will be picked.
-        If a voice's name is in VOICE_PREFERENCES this voice will read the text. 
+        """ Read given text. The dominant language will be determined to pick a suitable voice.
+        If a suitable voice's name is in VOICE_PREFERENCES this voice will read the text. 
         """
+        self.busy = True
         
         lng = self.detect_language(content)
         self.voices = self.get_voices(lng)
-        
         
         for voice in self.voices:
             self.language = str(voice.language())
@@ -65,25 +88,21 @@ class ClipboardReader:
             if self.name in VOICE_PREFERENCES:
                 self.id = str(voice.identifier())
                 break
-        
-        self.speak_with_voice(content, self.language, self.id)
-        
-        
-    def pause_speaking(self):
-        """Pause the speech synthesizer."""
-        if self.is_speaking():
-            self.SYNTHESIZER.pauseSpeakingAtBoundary_(0)  
-
-    def resume_speaking(self):
-        """Resume the speech synthesizer."""
-        if not self.is_speaking():
-            self.SYNTHESIZER.continueSpeaking()
+                
+        self.speak_with_voice(content)
         
     def is_speaking(self):
         return self.SYNTHESIZER.isSpeaking()
         
     def stop_speaking(self):
-        """Stops the speech synthesizer if it is currently speaking."""
+        """Stops the speech synthesizer if it is currently speaking.
+        """
         if self.is_speaking():
             self.SYNTHESIZER.stopSpeakingAtBoundary_(0)  # 0 steht für AVSpeechBoundaryImmediate
             
+    def update_speed_rate(self):
+        """Set voices speed to self.rate, a value from 0 to 1.
+        Updates apply to the next block.
+        """
+        self.UTTERANCE.setRate_(max(0, min(self.rate, 1)))
+        
